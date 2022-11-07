a010c98f5c9a854b78b20f5419ace6e13dddf0e1
YamilLazzariDicsys <yamil.lazzari@dicsys.com>
2022-11-07 20:06:50 -0300

    Create DTO Task

--- src/models/task/dto/create-task.dto.ts
+++ src/models/task/dto/create-task.dto.ts
@@ -1,1 +1,21 @@
-export class CreateTaskDto {}
+import { IsNotEmpty, IsString, IsNumber, Max, Min } from 'class-validator';
+export class CreateTaskDto {
+  @IsNumber()
+  id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @Min(5)
+  @Max(20)
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Min(5)
+  @Max(300)
+  description: string;
+
+  state: string;
+
+  category: string;
+}
