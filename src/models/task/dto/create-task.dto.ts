4862cb88990af7d5bb654ba3972280d600a57da1
YamilLazzariDicsys <yamil.lazzari@dicsys.com>
2022-11-08 08:51:56 -0300

    Integracion de Swagger

--- src/models/task/dto/create-task.dto.ts
+++ src/models/task/dto/create-task.dto.ts
@@ -1,18 +1,23 @@
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateTaskDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @MinLength(5, { message: 'El titulo debe ser mayor a 5 caracteres' })
   @MaxLength(20, { message: 'El titulo  debe ser menor a 30 caracteres' })
   title: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @MinLength(5, { message: 'La descripcion debe ser mayor a 5 caracteres' })
   @MaxLength(300, { message: 'La descripcion  debe ser menor a 30 caracteres' })
   description: string;
 
+  @ApiProperty()
   state: string;
 
+  @ApiProperty()
   category: string;
 }
